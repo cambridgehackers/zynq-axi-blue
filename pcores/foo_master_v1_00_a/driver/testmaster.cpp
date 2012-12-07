d055f1b7d5a4e2899197c567ea12b76d60265571
Jamey Hicks <jamey.hicks@nokia.com>
2012-12-07 17:09:28 -0500

    added command line params to control test

--- pcores/foo_master_v1_00_a/driver/testmaster.cpp
+++ pcores/foo_master_v1_00_a/driver/testmaster.cpp
@@ -1,6 +1,8 @@
 #include "ushw.h"
 #include "DUT.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 struct ResultIorMsg : public UshwMessage
 {
@@ -13,10 +15,41 @@ int responseChannel;
 
 DUT *dut = 0;
 
-int main(int argc, const char **argv)
+int main(int argc, char **argv)
 {
+    const char *devname = "foomaster0";
+    int config = 0;
+    int doWrite = 1;
+    int doRead = 1;
+    int verbose = 0;
+    int doTest = 0;
+    if (argc > 1) {
+        int opt;
+        while ((opt = getopt(argc, argv, "c:d:r:t:vw:")) != -1) {
+            switch (opt) {
+            case 'c':
+                config = atoi(optarg);
+                break;
+            case 'd':
+                devname = optarg;
+                break;
+            case 'r':
+                doRead = atoi(optarg);
+                break;
+            case 't':
+                doTest = atoi(optarg);
+                break;
+            case 'w':
+                doWrite = atoi(optarg);
+                break;
+            case 'v':
+                verbose = 1;
+                break;
+            }
+        }
+    }
     fprintf(stderr, "%s:%d\n", __FUNCTION__, __LINE__);
-    dut = DUT::createDUT("foomaster0");
+    dut = DUT::createDUT(devname);
     fprintf(stderr, "%s:%d dut=%p\n", __FUNCTION__, __LINE__, dut);
 
     unsigned long base = ushw.alloc(4096);
@@ -24,21 +57,34 @@ int main(int argc, const char **argv)
     dut->setBase(base);
     dut->setBounds(bound);
     dut->setEnabled(1);
-    if (1)
-    for (int w = 0; w < 16; w++) {
-      dut->enq(0x5ace0000 + w);
+    if (config)
+        dut->configure(config);
+    if (doTest) {
+        dut->runTest(doTest);
+        if (verbose)
+            for (int i = 0; i < 13; i++) {
+                dut->readFifoStatus(i*4);
+            }
+        if (verbose)
+            for (int i = 0; i < 13; i++) {
+                dut->readFromFifoStatus(i*4);
+            }
+    } else {
+        if (doWrite)
+            for (int w = 0; w < 16; w++) {
+                dut->enq(0x5ace0000 + w);
+            }
+        if (verbose)
+            for (int i = 0; i < 13; i++) {
+                dut->readFifoStatus(i*4);
+            }
+        if (doRead)
+            dut->readRange(base);
+        if (verbose)
+            for (int i = 0; i < 13; i++) {
+                dut->readFromFifoStatus(i*4);
+            }
     }
-    if (0)
-    for (int i = 0; i < 13; i++) {
-      dut->readFifoStatus(i*4);
-    }
-    if (1)
-    dut->readRange(base);
-    if (0)
-    for (int i = 0; i < 13; i++) {
-      dut->readFromFifoStatus(i*4);
-    }
-
     ushw.exec();
     fprintf(stderr, "%s:%d\n", __FUNCTION__, __LINE__);
     return 0;
